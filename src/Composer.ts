41b81e9a6038daf683dcff180041544c294d8672
ejnshtein <ejnshtein@gmail.com>
2020-12-05 04:00:32 +0200

    remove unused method Compose.getGenres, refactor Composer.getMangaLinks

--- src/Composer.ts
+++ src/Composer.ts
@@ -1,48 +1,22 @@
 import fs from 'fs'
 import { join } from 'path'
 import { links as linksData } from './lib/links'
-import { Link, PartialTag } from '../types/mangadex'
+import { Link } from '../types/mangadex'
 import { Language } from '../types/language'
 
-const genresData = JSON.parse(
-  fs.readFileSync(join(__dirname, '..', 'genres.json'), 'utf-8')
-)
 const langCodeData = JSON.parse(
   fs.readFileSync(join(__dirname, '..', 'langcodes.json'), 'utf-8')
 )
 
 export class Composer {
-  static getGenres(genres: number[]): PartialTag[] {
-    const res = genresData.filter((el) =>
-      genres.some((genreId) => el.id === genreId)
-    )
-    if (res.length >= 2) {
-      return res
-    } else {
-      return genres.map((genre) => ({
-        id: genre,
-        name: `Genre#${genre}`
-      }))
-    }
-  }
-
   static getMangaLinks(links: Record<string, string>): Link[] {
-    const formatted = Object.entries(links).map(([name, value]) => {
+    return Object.entries(links).map(([name, value]) => {
       const matchedLink = linksData[name]
-      if (matchedLink) {
-        return {
-          title: matchedLink.title,
-          url: matchedLink.url + value
-        }
-      } else {
-        return {
-          title: name,
-          url: value
-        }
+      return {
+        title: matchedLink ? matchedLink.title : name,
+        url: matchedLink ? matchedLink.url + value : value
       }
     })
-
-    return formatted
   }
 
   static getLangName(langCode: Language): string {
