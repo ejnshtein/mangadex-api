a65333d0d85b85abded4fc3d00d809fa17d2df4c
ejnshtein <ejnshtein@gmail.com>
2021-01-12 23:59:31 +0200

    add userFollowedPartialChapter schema

--- jest/schema/user.ts
+++ jest/schema/user.ts
@@ -1,6 +1,7 @@
 import Joi from 'joi'
 import {
   FollowedPartialManga,
+  FollowedPartialChapter,
   FollowedUpdates,
   User,
   UserManga,
@@ -8,7 +9,6 @@ import {
   UserSettings
 } from '../../types/mangadex'
 import { partialMangaSchema } from './manga'
-import { partialChapterSchema } from './chapter'
 import { partialBase } from './partial-base'
 
 export const userSchema = Joi.object<User>({
@@ -26,6 +26,26 @@ export const userSchema = Joi.object<User>({
   website: Joi.string().allow('').required()
 })
 
+export const userFollowedPartialChapterSchema = Joi.object<FollowedPartialChapter>(
+  {
+    chapter: Joi.string().allow('').required(),
+    comments: Joi.number().required(),
+    groups: Joi.array().items(Joi.number().required()).required(),
+    hash: Joi.string().required(),
+    id: Joi.number().required(),
+    language: Joi.string().required(),
+    mangaId: Joi.number().required(),
+    mangaTitle: Joi.string().allow(null).required(),
+    read: Joi.boolean().required(),
+    threadId: Joi.number().allow(null).required(),
+    timestamp: Joi.number().required(),
+    title: Joi.string().allow('').required(),
+    uploader: Joi.number().required(),
+    views: Joi.number().required(),
+    volume: Joi.string().allow(null, '').required()
+  }
+)
+
 export const userFollowedPartialMangaSchema = Joi.array().items(
   Joi.object<FollowedPartialManga>({
     chapter: Joi.string().allow('').required(),
@@ -40,7 +60,7 @@ export const userFollowedPartialMangaSchema = Joi.array().items(
 )
 
 export const userFollowedUpdatesSchema = Joi.object<FollowedUpdates>({
-  chapters: Joi.array().items(partialChapterSchema).required(),
+  chapters: Joi.array().items(userFollowedPartialChapterSchema).required(),
   groups: Joi.array().items(partialBase).required(),
   manga: Joi.array().items(partialMangaSchema).required()
 })
