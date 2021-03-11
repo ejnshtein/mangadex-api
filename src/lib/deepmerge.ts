7a39e88f4ed323d5a36122524c2abd12e0f7a9d6
ejnshtein <ejnshtein@gmail.com>
2021-03-12 00:42:26 +0200

    fix deepmerge settings properties on original target, instead of target copy

--- src/lib/deepmerge.ts
+++ src/lib/deepmerge.ts
@@ -2,28 +2,29 @@ export function deepmerge<T = Record<string, unknown>>(
   target: T,
   ...sources: T[]
 ): T {
+  const targetCopy = Object.assign({}, target)
   // eslint-disable-next-line @typescript-eslint/ban-types
   const isObject = (obj: object) => obj && typeof obj === 'object'
   sources.forEach((source) =>
     Object.keys(source).forEach((key) => {
-      const targetValue = target[key]
+      const targetValue = targetCopy[key]
       const sourceValue = source[key]
 
       if (Array.isArray(targetValue) && Array.isArray(sourceValue)) {
-        target[key] = targetValue.concat(sourceValue)
+        targetCopy[key] = targetValue.concat(sourceValue)
       } else if (
         isObject(targetValue) &&
         isObject(sourceValue as Record<string, unknown>)
       ) {
-        target[key] = deepmerge(
+        targetCopy[key] = deepmerge(
           Object.assign({}, targetValue),
           sourceValue as Record<string, unknown>
         )
       } else {
-        target[key] = sourceValue
+        targetCopy[key] = sourceValue
       }
     })
   )
 
-  return target
+  return targetCopy
 }
